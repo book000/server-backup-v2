593904335ca08858448a673608e82bdc41cf196e
Tomachi <tomachi@tomacheese.com>
2022-01-05 03:50:46 +0900

    Update rsync.sh

--- src/rsync.sh
+++ src/rsync.sh
@@ -29,11 +29,17 @@ TODAY=$(date +%Y-%m-%d)
 
 SSHCMD="rsync -arhvzsP --no-o --no-g --no-p --progress --delete --backup --exclude-from='${OUTPUT}/ignores' -e 'ssh -o StrictHostKeyChecking=no -p $PORT -i $IDENTITY' --rsync-path='sudo rsync' --backup-dir="${OUTPUT}/$TODAY" $USERNAME@$HOSTNAME:$FROM ${OUTPUT}/latest"
 expect -c "
-    set timeout 60
+    set timeout 30
     spawn sh -c \"$SSHCMD\"
-
-    expect \"Enter passphrase for key\" {
-        send \"$PASSPHRASE\n\"
+    expect {
+        \"Enter passphrase for key\" {
+            send -- \"$PASSPHRASE\n\"
+        }
+        \"total size is\" {
+            interact
+            expect eof
+            catch wait result
+        }
     }
     "
 echo rsync.sh end
