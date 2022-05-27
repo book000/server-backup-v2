cbe57b7fb8f21f4381ba3f0500667fc360aa4f4b
Tomachi <8929706+book000@users.noreply.github.com>
2022-05-27 10:56:33 +0900

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -217,7 +217,7 @@ class FullBackup(BaseBackup):
 
         os.unlink(os.path.join(BACKUP_DIR, "ignores"))
 
-        command = "tar cvf {0} {1}".format(
+        command = "tar cvf --remove-file {0} {1}".format(
             os.path.join(BACKUP_DIR, TODAY + ".tar.gz"),
             os.path.join(BACKUP_DIR, TODAY)
         )
