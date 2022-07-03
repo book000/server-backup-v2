c3fffa7b09b144831f194ec25e4a3610ed76de0e
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-07-04 05:14:34 +0900

    fix: remove commentout

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ if [ -z "$CONFIG_LOCATION" ]; then
     CONFIG_LOCATION="/data/config.json"
 fi
 
-# python -m src -f "${CONFIG_LOCATION}" -m "db"
+python -m src -f "${CONFIG_LOCATION}" -m "db"
 python -m src -f "${CONFIG_LOCATION}" -m "full"
